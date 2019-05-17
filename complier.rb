29a38d2c2c9b402c2a87e923ab3c8c705dec9397
Abhay Nikam <nikam.abhay1@gmail.com>
2019-05-17 22:33:28 +0530

    Second phase of the compiler - Parser

--- complier.rb
+++ complier.rb
@@ -7,7 +7,8 @@ class Tokenizer
     [:identifier, /\b[a-zA-Z]+\b/],
     [:integer,  /\b[0-9]+\b/],
     [:oparen, /\(/],
-    [:cparen, /\)/]
+    [:cparen, /\)/],
+    [:comma, /,/],
   ]
 
   def initialize(code)
@@ -44,3 +45,106 @@ Token = Struct.new(:type, :value)
 
 tokens = Tokenizer.new(File.read("test.src")).tokenize
 puts tokens.map(&:inspect).join("\n")
+
+class Parser
+  def initialize(tokens)
+    @tokens = tokens
+  end
+
+  def parse
+    parse_def
+  end
+
+  def parse_def
+    consume(:def)
+    name = consume(:identifier).value
+    arg_names = parse_arg_names
+    body = parse_expr
+    consume(:end)
+
+    DefNode.new(name, arg_names, body)
+  end
+
+  def parse_arg_names
+    arg_names = []
+    consume(:oparen)
+
+    if peek(:identifier)
+      arg_names << consume(:identifier).value
+
+      while peek(:comma)
+        consume(:comma)
+        arg_names << consume(:identifier).value
+      end
+    end
+
+    consume(:cparen)
+    arg_names
+  end
+
+  def parse_expr
+    if peek(:integer)
+      parse_integer
+    elsif peek(:identifier) && peek(:oparen, 1)
+      parse_call
+    else
+      parse_var_ref
+    end
+  end
+
+  def parse_call
+    name = consume(:identifier).value
+    arg_exprs = parse_exprs
+
+    CallNode.new(name, arg_exprs)
+  end
+
+  def parse_exprs
+    arg_exprs = []
+    consume(:oparen)
+
+    if !peek(:cparen)
+      arg_exprs << parse_expr
+
+      while peek(:comma)
+        consume(:comma)
+        arg_exprs << parse_expr
+      end
+    end
+
+    consume(:cparen)
+    arg_exprs
+  end
+
+  def parse_integer
+    IntegerNode.new(consume(:integer).value.to_i)
+  end
+
+  def consume(expected_type)
+    token = @tokens.shift
+
+    if token.type == expected_type
+      token
+    else
+      raise RuntimeError.new(
+        "Expected token type #{expected_type.inspect} but got #{token.type.inspect}"
+      )
+    end
+  end
+
+  def parse_var_ref
+    VarRefNode.new(consume(:identifier).value)
+  end
+
+  def peek(expected_type, offset = 0)
+    @tokens.fetch(offset).type == expected_type
+  end
+end
+
+DefNode = Struct.new(:name, :arg_names, :body)
+IntegerNode = Struct.new(:value)
+CallNode = Struct.new(:name, :arg_exprs)
+VarRefNode = Struct.new(:value)
+
+tree = Parser.new(tokens).parse
+p tree
